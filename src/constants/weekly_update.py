22ef75df7f7de1129aab567fade3b7b83c61365d
Matthew Grosman <grosmanmatt@gmail.com>
2023-01-16 19:04:14 -0800

    Fixed local path referencing in weekly_update.py

--- src/constants/weekly_update.py
+++ src/constants/weekly_update.py
@@ -1,11 +1,11 @@
 import random
-import src.constants.trivia as frank_trivia
 
+from .trivia import TRIVIA
 from src import utils
 
 
 WEEKLY_UPDATE_MESSAGE = "**HELLO FELLOW FRANK OCEAN FANS!!!!** It is time for another weekly Frank Ocean fun " \
-                f"fact!\n\n*Frank Ocean fun fact of the day*: {random.choice(frank_trivia.TRIVIA)} \n\nWow, what an " \
+                f"fact!\n\n*Frank Ocean fun fact of the day*: {random.choice(TRIVIA)} \n\nWow, what an " \
                 f"incredible fact. And now it's time for the **WEEKLY COACHELLA COUNTDOWN**: There are " \
                 f"only **{utils.get_days_until_coachella()} days left** until Frank Ocean performs at Coachella!!! " \
                 f"FRANKCHELLA APPROACHES SOON.\n\nHave a fantastic day everyone :)"
