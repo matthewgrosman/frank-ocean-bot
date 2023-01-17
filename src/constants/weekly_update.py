0e2356865427d20e47efe60a2a9ce0af0633f171
Matthew Grosman <grosmanmatt@gmail.com>
2023-01-16 19:16:33 -0800

    Fixed relative import issue for date util function

--- src/constants/weekly_update.py
+++ src/constants/weekly_update.py
@@ -1,11 +1,12 @@
 import random
+import datetime
 
 from .trivia import TRIVIA
-from src import utils
+from .coachella import COACHELLA_FRANK_DATE
 
 
 WEEKLY_UPDATE_MESSAGE = "**HELLO FELLOW FRANK OCEAN FANS!!!!** It is time for another weekly Frank Ocean fun " \
                 f"fact!\n\n*Frank Ocean fun fact of the day*: {random.choice(TRIVIA)} \n\nWow, what an " \
                 f"incredible fact. And now it's time for the **WEEKLY COACHELLA COUNTDOWN**: There are " \
-                f"only **{utils.get_days_until_coachella()} days left** until Frank Ocean performs at Coachella!!! " \
-                f"FRANKCHELLA APPROACHES SOON.\n\nHave a fantastic day everyone :)"
+                f"only **{(COACHELLA_FRANK_DATE - datetime.date.today()).days} days left** until Frank Ocean performs " \
+                f"at Coachella!!! FRANKCHELLA APPROACHES SOON.\n\nHave a fantastic day everyone :)"
